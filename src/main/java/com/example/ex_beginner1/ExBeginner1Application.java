0f23258fe2473305957e7a89e88bddf6d2e1e9d5
Sato <yusuke.sato@rakus-partners.co.jp>
2024-08-22 18:29:00 +0900

    first commit

---
+++ src/main/java/com/example/ex_beginner1/ExBeginner1Application.java
@@ -0,0 +1,13 @@
+package com.example.ex_beginner1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExBeginner1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExBeginner1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ex_beginner1/ExBeginner1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ex_beginner1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExBeginner1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
